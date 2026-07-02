beccbaab1d9804a527fbda637b588189031a6e26
VEEETOOORRR <antonni2601.ag@gmail.com>
2026-07-01 22:36:39 -0300

    Módulo comparador finalizado

---
+++ rtl/comparator.sv
@@ -0,0 +1,13 @@
+module comparator ( 
+    input logic [7:0] credit,
+    input logic [7:0] price,
+    input logic [7:0] stock,
+
+    output logic can_sell
+);
+
+    // Módulo puramente combinacional
+    assign can_sell = (credit >= price) && (stock > 8'b0);
+
+
+endmodule
